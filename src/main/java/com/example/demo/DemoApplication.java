901356d3843303a905535d78f099332307728632
anizm <anizm.nizar@3ktechnologies.com>
2019-02-09 13:35:11 +0530

    Redeploy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer   {
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
 	}
@@ -19,6 +21,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
 }
 
